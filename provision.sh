c596a0e9bf09858a3049bf7c83f3b726cb6d7271
Alessandro Melchiori <alessandro.melchiori@codiceplastico.com>
2020-09-19 14:09:00 +0200

    install default packages and enable UI

--- provision.sh
+++ provision.sh
@@ -15,3 +15,34 @@ if ! id -u melkio &>/dev/null; then
             melkio
     echo "melkio ALL=(ALL) NOPASSWD:ALL" | sudo tee -a /etc/sudoers.d/99_melkio > /dev/null
 fi
+
+echo ">>>> Update packages"
+apt-get update
+apt-get upgrade -y
+
+echo ">>>> Install basic packages"
+apt-get install \
+    build-essential autoconf \
+    -y
+apt-get install \
+    firefox-esr gnome-terminal \
+    git tree unzip jq \
+    curl wget httpie \
+    -y
+
+echo ">>>> Install GUI packages"
+apt-get install xorg i3 slim -y
+
+# Other packages to install (evaluate)
+# libreadline-dev
+# suckless-tools
+# xclip x11-utils autocutsel unclutter
+# dbus-x11 libglib2.0-bin
+
+echo ">>>> Configure and start slim..."
+cp -r /vagrant/config/slim/greeny_dark /usr/share/slim/themes/
+cp /vagrant/config/slim/slim.conf /etc/slim.conf
+
+if service slim status | grep inactive; then
+    service slim start
+fi
