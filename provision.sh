6c07450933b22de9fa9980afe092553da651e8b8
Alessandro Melchiori <alessandro.melchiori@codiceplastico.com>
2020-09-19 14:09:43 +0200

    install vscode

--- provision.sh
+++ provision.sh
@@ -46,3 +46,14 @@ cp /vagrant/config/slim/slim.conf /etc/slim.conf
 if service slim status | grep inactive; then
     service slim start
 fi
+
+echo ">>>> Install VS Code"
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
+sh -c 'echo "deb [arch=amd64 signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+
+apt-get install apt-transport-https -y
+apt-get update
+apt-get install code -y
+
+echo ">>>> That's all, rock on!"
