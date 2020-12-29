03b2b4fd91a0dbaa5fc24a9b3eb93bdb207f718f
Alessandro Melchiori <alessandro.melchiori@codiceplastico.com>
2020-12-29 23:04:24 +0100

    add erlang asdf plugin

--- provision.sh
+++ provision.sh
@@ -97,6 +97,7 @@ sudo -iu melkio <<HEREDOC
     source ~/.asdf/asdf.sh
 
     asdf plugin-add dotnet-core https://github.com/emersonsoares/asdf-dotnet-core.git
+    asdf plugin-add erlang https://github.com/asdf-vm/asdf-erlang.git
     asdf plugin-add nodejs https://github.com/asdf-vm/asdf-nodejs.git
     bash -c '~/.asdf/plugins/nodejs/bin/import-release-team-keyring'
 
